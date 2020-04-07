a1f6841aef2c4ec07e457710149fff8fb5861645
Maksym Kruhovykh <JavaCat2019@gmail.com>
2020-04-07 11:10:47 +0000

    Add moder for sing in/sing up

---
+++ src/main/java/my/makrket/model/LoginRequestModel.java
@@ -0,0 +1,13 @@
+package my.makrket.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LoginRequestModel {
+
+    private  String email;
+    private  String password;
+
+}
